27a4bad28a8adccfd311f60dc18f155f515adcbd
dlkozan <145031313+dlkozan@users.noreply.github.com>
2025-03-20 11:31:38 -0300

    .

--- script.js
+++ script.js
@@ -36,3 +36,10 @@ document.addEventListener('DOMContentLoaded', function(){
  
  
  })
+
+})
+ 
+ScrollReveal().reveal('#inicio', { delay: 500 });
+ScrollReveal().reveal('#tropicalia', { delay: 500 });
+ScrollReveal().reveal('#galeria', { delay: 500 });
+ScrollReveal().reveal('#contato', { delay: 500 });
